b3e2185c66920743982f24b47b820789198534bb
Nicolas <pereznicolas90@gmail.com>
2019-01-27 18:20:18 -0500

    Subiendo funcion idprocessor
    Para encontrar idprocessor (nombre de modelo)

---
+++ idprocessor/idprocessor.go
@@ -0,0 +1,10 @@
+package idprocessor
+
+import (
+	"github.com/jpoirier/cpu"
+)
+// GetProcessorID retorna el id del procesador
+func GetProcessorID() (idprocessor string){
+	idprocessor = cpu.ProcessorFamily
+	return
+}
